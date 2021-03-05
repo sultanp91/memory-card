4283b7f4f1c696080c1d916a9f51ff47c959f4ca
Sultan <sultanp@gmail.com>
2021-03-05 20:54:29 +0000

    shuffle and count features added

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,16 +1,43 @@
 import React, { useState } from 'react';
+import uniqid from 'uniqid';
 
 function Container() {
 
         const [ cards, setCards ] = useState([
-            {card: "img1", count: 0},
-            {card: "img2", count: 0},
-            {card: "img3", count: 0},
-            {card: "img4", count: 0}]);
+            {card: "img1", count: 0, id: uniqid()},
+            {card: "img2", count: 0, id: uniqid()},
+            {card: "img3", count: 0, id: uniqid()},
+            {card: "img4", count: 0, id: uniqid()}]);
+
+        const shuffle = (array) => {
+            for (let i = array.length - 1; i > 0; i--) {
+                let j = Math.floor(Math.random() * (i + 1));
+                [array[i], array[j]] = [array[j], array[i]];
+            }
+            }
+
+        const playRound = (evt) => { 
+            const card = Object.assign([], cards);
+            card.forEach((item) => {
+               if(item.id === evt.target.id){
+                    item.count = item.count + 1
+               }
+           })
+
+           shuffle(card)
+
+           setCards(card)
+        }
+    
+    
 
     return (
         <div>
-            {cards.map((item) => <p>{item.card} - {item.count}</p>)}
+            {cards.map((item) => ( 
+                <p onClick={playRound} 
+                id={item.id}>
+                {item.card} - {item.count}
+                </p>))}
         </div>
     )
 }

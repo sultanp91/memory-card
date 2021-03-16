01d74b084f8a7d90c765a77322b4ec0df784e082
Sultan <sultanp@gmail.com>
2021-03-16 18:37:17 +0000

    updated click event to div instead of img

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,11 +2,12 @@ import React from "react";
 
 function Card({ card, playRound }) {
   return (
-    <div className="card-div">
-      <img
+    <div className="card-div"
         onClick={playRound}
         key={card}
         id={card}
+    >
+      <img
         src={`./img/${card}.png`}
         alt={card}
         className="card-image"

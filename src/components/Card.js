dd69a504c2a7314e74722421544c2dbd3852ebb8
Sultan <sultanp@gmail.com>
2021-03-16 19:05:16 +0000

    changed event delegation on card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ import React from "react";
 function Card({ card, playRound }) {
   return (
     <div className="card-div" onClick={playRound} data-card={card}>
-      <img src={`./img/${card}.png`} onClick={playRound} data-card={card} alt={card} className="card-image" />
+      <img src={`./img/${card}.png`} data-card={card} alt={card} className="card-image" />
     </div>
   );
 }

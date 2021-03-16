988d5438b552ca127634e82ac3f6d3f3cd056b73
Sultan <sultanp@gmail.com>
2021-03-16 18:41:13 +0000

    updated formatting

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,16 +2,8 @@ import React from "react";
 
 function Card({ card, playRound }) {
   return (
-    <div className="card-div"
-        onClick={playRound}
-        key={card}
-        id={card}
-    >
-      <img
-        src={`./img/${card}.png`}
-        alt={card}
-        className="card-image"
-      />
+    <div className="card-div" onClick={playRound} key={card} id={card}>
+      <img src={`./img/${card}.png`} alt={card} className="card-image" />
     </div>
   );
 }
